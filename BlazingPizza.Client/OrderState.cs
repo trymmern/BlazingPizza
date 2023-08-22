a451c5db1f1d9871007f6c073441b43cfad91204
Trym Todalshaug <trymrt@gmail.com>
2023-08-22 14:49:54 +0200

    Improve app state. Finished 04

---
+++ BlazingPizza.Client/OrderState.cs
@@ -0,0 +1,49 @@
+namespace BlazingPizza.Client
+{
+    public class OrderState
+    {
+        public bool ShowConfiguringDialog { get; private set; }
+        public Pizza ConfiguringPizza { get; private set; }
+        public Order Order { get; private set; } = new();
+        
+        public void ShowConfiguringPizzaDialog(PizzaSpecial special)
+        {
+            ConfiguringPizza = new Pizza
+            {
+                Special = special,
+                SpecialId = special.Id,
+                Size = Pizza.DefaultSize,
+                Toppings = new List<PizzaTopping>()
+            };
+
+            ShowConfiguringDialog = true;
+        }
+        
+        public void CancelConfigurePizzaDialog()
+        {
+            ResetConfiguringPizza();
+        }
+
+        public void ConfirmConfigurePizzaDialog()
+        {
+            Order.Pizzas.Add(ConfiguringPizza);
+            ResetConfiguringPizza();
+        }
+
+        public void RemoveConfiguredPizza(Pizza pizza)
+        {
+            Order.Pizzas.Remove(pizza);
+        }
+
+        public void ResetOrder()
+        {
+            Order = new Order();
+        }
+        
+        private void ResetConfiguringPizza()
+        {
+            ConfiguringPizza = null;
+            ShowConfiguringDialog = false;
+        }
+    }
+}
